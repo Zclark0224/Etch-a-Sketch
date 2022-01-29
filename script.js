7397fafe2ded8813d932b7fa1405cd3605820d4a
Zach Clark <zclark0224@gmail.com>
2022-01-29 09:45:23 -0500

    create the grid and general layout of Etch-a-Sketch

---
+++ script.js
@@ -0,0 +1,14 @@
+const container = document.getElementById('container');
+
+
+function makeRows(rows, columns) {
+    container.style.setProperty('--grid-rows', rows);
+    container.style.setProperty('--grid-columns', columns);
+    for (c=0; c < (rows*columns); c++) {
+        let cell = document.createElement('div');
+        //cell.innerText = c + 1;
+        container.appendChild(cell).className = 'grid-item';
+    }
+}
+
+makeRows(16,16)
