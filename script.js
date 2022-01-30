f68e08cd745fd7587050d94df1ed1552b17eec6b
Zach Clark <zclark0224@gmail.com>
2022-01-30 10:41:38 -0500

    add all elements to make etch-a-sketch work and add organization

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-const defaultColor = 'black';
+//defaults
+const defaultColor = '#000000';
 const defaultMode = 'color';
 const defaultSize = 16;
 
@@ -6,6 +7,7 @@ let currentColor = defaultColor;
 let currentMode = defaultMode;
 let currentSize = defaultSize;
 
+//elements from HTML
 const container = document.getElementById('container');
 const gridItem = document.querySelector('.grid-item');
 const colorBtn = document.querySelector('.colorBtn');
@@ -13,22 +15,37 @@ const rainbowBtn = document.querySelector('.rainbowBtn');
 const eraserBtn = document.querySelector('.eraserBtn');
 const clearBtn = document.querySelector('.clearBtn');
 const charcoalBtn = document.querySelector('.charcoalBtn');
+const colorPicker = document.querySelector('#colorPicker');
+const slider = document.getElementById('sizeSlider');
+const sizeDisplay = document.getElementById('sizeDisplay');
 
+//event listeners
+colorBtn.addEventListener('click', function() {
+    setCurrentMode('color')
+});
+eraserBtn.addEventListener('click', function() {
+    setCurrentMode('eraser');
+});
+rainbowBtn.addEventListener('click', function() {
+    setCurrentMode('rainbow');
+});
+charcoalBtn.addEventListener('click', function() {
+    setCurrentMode('charcoal');
+});
+slider.addEventListener('change', function(){
+    sizeDisplay.innerHTML = `${this.value} x ${this.value}`;
+    container.innerHTML='';
+    setCurrentSize(this.value);
+    makeGrid(this.value);
+});
 clearBtn.addEventListener('click', function() {
     container.innerHTML='';
     makeGrid(currentSize);
 });
+colorPicker.addEventListener('change', newColor, false);
+colorPicker.value = defaultColor;
 
-colorBtn.onclick = () => setCurrentMode('color');
-eraserBtn.onclick = () => setCurrentMode('eraser');
-rainbowBtn.onclick = () => setCurrentMode('rainbow');
-charcoalBtn.onclick = () => setCurrentMode('charcoal');
-
-function setCurrentMode(newMode) {
-    activateNewMode(newMode);
-    currentMode = newMode;
-}
-
+//size functions
 function makeGrid(size) {
     container.style.gridTemplateColumns = `repeat(${size}, 1fr)`
     container.style.gridTemplateRows = `repeat(${size}, 1fr)`
@@ -37,6 +54,47 @@ function makeGrid(size) {
         container.appendChild(cell);
         cell.addEventListener('mouseover', changeColor);
     }
+};
+
+function setCurrentSize(newSize) {
+    currentSize = newSize;
+};
+
+function updateSizeDisplay(newSize) {
+    sizeDisplay.innerHTML = `${value} x ${value}`;
+};
+
+//mode functions
+function setCurrentMode(newMode) {
+    activateNewMode(newMode);
+    currentMode = newMode;
+};
+
+function activateNewMode(newMode) {
+    if (currentMode === 'rainbow') {
+        rainbowBtn.classList.remove('active');
+    } else if (currentMode === 'color'){
+        colorBtn.classList.remove('active');
+    } else if (currentMode === 'eraser') {
+        eraserBtn.classList.remove('active');
+    } else if (currentMode === 'charcoal') {
+        charcoalBtn.classList.remove('active');
+    };
+
+    if (newMode === 'rainbow') {
+        rainbowBtn.classList.add('active');
+    } else if (newMode === 'color'){
+        colorBtn.classList.add('active');
+    } else if (newMode === 'eraser') {
+        eraserBtn.classList.add('active');
+    } else if (newMode === 'charcoal') {
+        charcoalBtn.classList.add('active');
+    };
+};
+
+//color functions
+function newColor(event) {
+    currentColor = event.target.value;
 }
 
 function changeColor(e){
@@ -49,33 +107,13 @@ function changeColor(e){
         e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`;
     } else if (currentMode === 'eraser') {
         e.target.style.backgroundColor = '#FEFEFE';
-    } /*else if (currentMode === 'charcoal') {
-        e.target.style.backgroundColor = 
-   }*/
-}
-
-function activateNewMode(newMode) {
-    if (currentMode === 'rainbow') {
-        rainbowBtn.classList.remove('active');
-    } else if (currentMode === 'color'){
-        colorBtn.classList.remove('active');
-    } else if (currentMode === 'eraser') {
-        eraserBtn.classList.remove('active');
     } else if (currentMode === 'charcoal') {
-        charcoalBtn.classList.remove('active');
-    }
-
-    if (newMode === 'rainbow') {
-        rainbowBtn.classList.add('active');
-    } else if (newMode === 'color'){
-        colorBtn.classList.add('active');
-    } else if (newMode === 'eraser') {
-        eraserBtn.classList.add('active');
-    } else if (newMode === 'charcoal') {
-        charcoalBtn.classList.add('active');
-    }
+        e.target.style.backgroundColor = 'black';
+        e.target.style.opacity = (parseFloat(e.target.style.opacity) || 0) + 0.2;
+   }
 }
 
+//onload
 window.onload = function() {
     makeGrid(defaultSize);
     activateNewMode('color');
